e00c6e3c6052f1acf57ab6ac4c364108150a31d0
NikoWZwane <williezn92@gmail.com>
2023-06-20 03:50:44 -0700

    Preprocessor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+ 
+  // declare strings
+  char str[] = "Programming is like building a multilingual puzzle\n";
+ 
+  // print output using fuction puts
+  puts(str);
+
+  return 0;
+}
