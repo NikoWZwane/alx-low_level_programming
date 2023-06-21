dc45811af3147461db947e206381433f8c580021
NikoWZwane <williezn92@gmail.com>
2023-06-21 06:57:31 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
  * main - Write a C program that prints the size of various typeis
- * Return:0 Always (success)
  *
-*/
+ * Return: Always 0 (success)
+ **/
 int main(void)
 {
-char charType;
-int intType;
-long longType;
-long int longInt;
-float floatType;
-long long int longLongInt;
-printf("Size of a char:%zu byte(s)\n", sizeof(charType));
-printf("Size of an int:%zu byte(s)\n", sizeof(intType));
-printf("size of a long:%zu byte(s)\n", sizeof(longType));
-printf("Size of a long int:%zu byte(s)\n", sizeof(longInt));
-printf("Size of a long long int:%zu byte(s)\n", sizeof(longLongInt));
-Printf("Size of a float:%zu byte(s)\n", sizeof(floatType));
+	{	char charType;
+		int intType;
+		long longType;
+		long int longInt;
+		float floatType;
+		long long int longLongInt;
+	}
+	printf("Size of a char:%zu byte(s)\n", sizeof(charType));
+	printf("Size of an int:%zu byte(s)\n", sizeof(intType));
+	printf("size of a long:%zu byte(s)\n", sizeof(longType));
+	printf("Size of a long int:%zu byte(s)\n", sizeof(longInt));
+	printf("Size of a long long int:%zu byte(s)\n", sizeof(longLongInt));
+	Printf("Size of a float:%zu byte(s)\n", sizeof(floatType));
 }
-return(0);
+return (0);
