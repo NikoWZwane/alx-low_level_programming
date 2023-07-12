aaf04df94b10c773d5a646639f4817c77fb43d9b
NikoWZwane <williezn92@gmail.com>
2023-07-12 13:48:44 -0700

    reverses the content of an array of integers

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -30,7 +30,7 @@ void print_array(int *a, int n)
  *
  * Return: Always 0.
  */
-i`nt main(void)
+int main(void)
 {
 	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int tmpo, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmpo = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmpo;
+	}
+}
