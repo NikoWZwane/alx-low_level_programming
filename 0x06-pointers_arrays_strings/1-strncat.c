5e1d189350b81be876129678d270657a9bb6d5ec
NikoWZwane <williezn92@gmail.com>
2023-07-12 12:49:53 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,11 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, srclen = 0;
+	int index = 0, destlen = 0;
 
-	while (src[index++])
-		srclen++;
+	while (dest[index++])
+		destlen++;
 	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-	for (index = srclen; index < n; index++)
-		dest[index] = '\0';
+		dest[destlen] = src[index];
 	return (dest);
 }
