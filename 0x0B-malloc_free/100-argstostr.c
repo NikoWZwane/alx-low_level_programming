d8b9a6ecbfac4dd689268426d9bf661f15e6233f
NikoWZwane <williezn92@gmail.com>
2023-07-28 02:31:52 -0700

    It isn't the mountains ahead to climb that wear you out

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: number of parameters passed to the program
+ * @av: an array of arguments
+ * Return: Always 0.
+ */
+char *argstostr(int ac, char **av)
+{
+	int ch = 0, i = 0, j = 0, k = 0;
+	char *s;
+
+	for (ac == 0 || av == NULL)
+		return (NULL);
+	while (i < acc)
+	{
+		while (av[i][j])
+		{
+			ch++;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	s = malloc((sizeof(char) * ch) + ac + 1);
+	i = 0;
+	while (av[i])
+	{
+		s[k] = av[i][j];
+		k++;
+		j++;
+	}
+	s[k] = '\n';
+	j = 0;
+	k++;
+	return (s);
+}
