0719d7869c7298010b9c52459fe9ff37c2dc3d42
NikoWZwane <williezn92@gmail.com>
2023-08-30 12:27:36 -0700

    C - Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - string is a palindrome
+ * @s: string
+ * @n1: number 1 for iterator
+ * @n2: number 2 for iterator
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s = 1));
+}
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
