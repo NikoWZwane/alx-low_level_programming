e16da89de05ec9c999bcb989e989dec130598b3a
NikoWZwane <williezn92@gmail.com>
2023-09-05 03:42:57 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,7 +15,7 @@ int word_len(char *str)
 {
 	int index = 0, len = 0;
 
-	while (*(cstr + index) && *(str + index) != ' ')
+	while (*(str + index) && *(str + index) != ' ')
 	{
 		len++;
 		index++;
