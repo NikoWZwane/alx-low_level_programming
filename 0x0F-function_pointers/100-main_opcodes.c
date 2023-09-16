58a62e1ecd0aae204d511170ff510ace1d61d4ad
NikoWZwane <williezn92@gmail.com>
2023-09-16 08:33:36 -0700

    the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -28,7 +28,11 @@ int main(int argc, char *argv[])
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
-			printf("%02hhx ", arr[i]);
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
