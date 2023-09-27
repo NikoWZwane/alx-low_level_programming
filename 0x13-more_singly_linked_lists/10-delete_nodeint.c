6bb3d8bfbc88d991d92eb4fc2b1a5180f5ec85a6
NikoWZwane <williezn92@gmail.com>
2023-09-27 06:38:43 -0700

    Insert

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: double pointer to list file
+ * @index: node to be deleted
+ *
+ * Return: 1 if it succeeded, -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int ind;
+	listint_t *tmp = *head;
+	listint_t *nex_node = NULL;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(tmp);
+
+		return (1);
+	}
+	while (ind < index - 1)
+	{
+		if (!tmp || !(tmp->next))
+			return (-1);
+		tmp = tmp->next;
+		ind++;
+	}
+	nex_node = tmp->next;
+	free(nex_node);
+
+	return (1);
+}
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,11 +1,40 @@
 #include "lists.h"
 /**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @idx: is the index of the list where the new node
+ * @n: integer pointed to lists file
+ * @head: double pointer to lists file
  *
- *
- *
- *
- *
+ * Return: New node
  */
-nt delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int ind;
+	listint_t *new_node;
+	listint_t *tmp = *head;
 
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node || !head)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	for (ind = 0; tmp && ind < idx; ind++)
+	{
+		if (ind == idx - 1)
+		{
+			new_node->next = tmp->next;
+			tmp->next = new_node;
+			return (new_node);
+		}
+		else
+			tmp = tmp->next;
+	}
+	return (NULL);
+}
