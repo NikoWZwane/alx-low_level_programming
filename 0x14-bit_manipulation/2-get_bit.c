ff3db734f944406bfce2d3356972b383c3b1a236
NikoWZwane <williezn92@gmail.com>
2023-10-03 06:41:06 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit -
+ * @index: is the index, starting from 0
+ * @n: number of bit
+ *
+ * Retrun: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_value;
+
+	if (index > 63)
+		return (-1);
+	bit_value = (n >> index) & 1;
+	return (bit_value);
+}
