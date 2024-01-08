d9085fc646dbfea8f6581d48938c4550a3fb5c02
NikoWZwane <williezn92@gmail.com>
2024-01-08 02:41:14 -0800

    Delete an element at the beginning of a list.

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,25 +12,34 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *previous = NULL;
 	unsigned int i = 0;
 
-	while (current != NULL)
-	{
-	if (i == index)
-	{
-		if (previous == NULL)
-		*head = current->next;
+	if (*head == NULL)
+	return (-1);
 
-		previous->next = current->next;
-
-		if (current->next != NULL)
-		current->next->prev = previous;
+	if (index == 0)
+	{
+	*head = (*head)->next;
+	if (*head != NULL)
+		(*head)->prev = NULL;
 		free(current);
 	return (1);
 	}
 
+	while (current != NULL && i < index)
+	{
 		previous = current;
 		current = current->next;
 		i++;
 	}
 
+	if (current == NULL)
 	return (-1);
+
+	previous->next = current->next;
+
+	if (current->next != NULL)
+	current->next->prev = previous;
+
+	free(current);
+	return (1);
 }
+
